db8b5a0eb4dd5c19ba36c9053ef01ba414a82c1a
PabloNavarroAndres <pablo.navarro.andres01@gmail.com>
2024-01-31 22:10:45 +0100

    ControladorFalleros con mostrarFallero() modificado, ahora la clase Fallero obtiene los datos de la BD y no el controlador, que solo devuelve el objeto fallero creado en la funcion

--- controlador/ControladorFalleros.php
+++ controlador/ControladorFalleros.php
@@ -1,39 +1,32 @@
 <?php 
 
-include("Modelo.php");
-include("Fallero.php");
-function mostrarFallero($dni) {
+require "modelo/Modelo.php";
+require "modelo/Fallero.php";
+
+class ControladorFalleros {
+
+    /* ************************************************************************************ *
+	 * CONSTRUCTOR
+	 * ************************************************************************************ */
+	function __construct() {}	
+
+
+
+	/* *******************************************************************************************
+	 * METODOS PUBLICOS
+	 * ***************************************************************************************** */	
+    public function mostrarFallero() {
+        // dni ejemplo: 78901234G //
+        $dni = $_POST['dni'];
+
+        $fallero = new Fallero($dni);
     
-    // Clase de Modelo donde esta la conexion a la BD
-    $modelo = new Modelo();
+        // Obtener el fallero de su clase
+        $fallero->getFallero();
 
-    // Conexion a la BD
-    $bd = $modelo->conn;
-
-    // SQL para mostrar falleros- > dni, nombre, apellidos, cuota, id_falla
-
-    // Query fallero
-    $sqlFallero = (
-        "SELECT dni, nombre, apellidos, cuota, id_falla
-        FROM falleros"
-    );
-
-    // Preparamos la consulta
-    $sqlFallero = $bd->query($sqlFallero);
-
-    // Bucle para sacar datos de cada fallero: dni, nombre, apellidos, cuota, id_falla
-    while ($fallero = $sqlFallero->fetch()) {
-
-        // Objeto fallero
-        $fallero = new Fallero($fallero['dni'], $fallero['nombre'], $fallero['apellidos'], $fallero['cuota'], $fallero['id_falla']);
-
-        // Array falleros
-        $arrFalleros[] = $fallero;
+        // Devolvemos el fallero
+        return $fallero;
     }
-
-    // Devolvemos el array de fallas
-    return $arrFalleros;
-
 
 }
 
